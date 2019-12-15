84ded4a89abeafa6ad2374a6c598daf02b35b7fb
Mitsunobu ARAKI <45710134+araki-m@users.noreply.github.com>
2019-12-15 20:31:32 +0900

    Update sx.py

--- sx.py
+++ sx.py
@@ -7,7 +7,7 @@ N_CNT = 20
 M_CNT = 24
 
 MIN_NUM = 1
-MAX_NUM = 20
+MAX_NUM = 15
 
 MAX_CT = 3 
 MAX_RT = 7 
@@ -25,7 +25,9 @@ def main(stdscr):
     m = M_CNT
 
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
+#1st
     while(True):
 
         for i in range(MIN_NUM, MAX_NUM):
@@ -74,12 +76,17 @@ def main(stdscr):
             time.sleep(0.2)
 
         #stdscr.getkey()
- 
-        for i in range(MIN_NUM, MAX_NUM):
 
-            n =  n + 1
+#1st-2
+        for i in range(1, 5):
+
+	    if n == 0:
+                break
+
+            n =  n - 1
 
             stdscr.clear()
+
             stdscr.addstr(n, m, 'X')
             stdscr.addstr(n, m+1, 'X')
             stdscr.addstr(n+1, m, 'X')
@@ -111,6 +118,69 @@ def main(stdscr):
             stdscr.addstr(n+2, m+1+MAX_RT, 'X')
             stdscr.addstr(n+3, m+MAX_RT, 'X',curses.color_pair(1))
             stdscr.addstr(n+3, m+1+MAX_RT, 'X',curses.color_pair(1))
+
+            stdscr.refresh()
+
+            time.sleep(0.2)
+
+        #stdscr.getkey()
+
+#2nd-before
+ 
+        for i in range(1, 5):
+
+            n =  n + 1
+
+            stdscr.clear()
+            stdscr.addstr(n, m, 'X')
+            stdscr.addstr(n, m+1, 'X')
+            stdscr.addstr(n+1, m, 'X')
+            stdscr.addstr(n+1, m+1, 'X')
+            stdscr.addstr(n+2, m, 'X')
+            stdscr.addstr(n+2, m+1, 'X')
+            stdscr.addstr(n+3, m, 'X',curses.color_pair(2))
+            stdscr.addstr(n+3, m+1, 'X',curses.color_pair(2))
+
+#
+
+            stdscr.addstr(n, m+MAX_RT, 'X')
+            stdscr.addstr(n, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+1, m+MAX_RT, 'X')
+            stdscr.addstr(n+1, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+2, m+MAX_RT, 'X')
+            stdscr.addstr(n+2, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+3, m+MAX_RT, 'X',curses.color_pair(2))
+            stdscr.addstr(n+3, m+1+MAX_RT, 'X',curses.color_pair(2))
+
+            stdscr.refresh()
+
+            time.sleep(0.3)
+
+#2nd
+        for i in range(MIN_NUM, MAX_NUM):
+
+            n =  n + 1
+
+            stdscr.clear()
+            stdscr.addstr(n, m, 'X')
+            stdscr.addstr(n, m+1, 'X')
+            stdscr.addstr(n+1, m, 'X')
+            stdscr.addstr(n+1, m+1, 'X')
+            stdscr.addstr(n+2, m, 'X')
+            stdscr.addstr(n+2, m+1, 'X')
+            stdscr.addstr(n+3, m, 'X',curses.color_pair(2))
+            stdscr.addstr(n+3, m+1, 'X',curses.color_pair(2))
+
+#
+
+            stdscr.addstr(n, m+MAX_RT, 'X')
+            stdscr.addstr(n, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+1, m+MAX_RT, 'X')
+            stdscr.addstr(n+1, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+2, m+MAX_RT, 'X')
+            stdscr.addstr(n+2, m+1+MAX_RT, 'X')
+            stdscr.addstr(n+3, m+MAX_RT, 'X',curses.color_pair(2))
+            stdscr.addstr(n+3, m+1+MAX_RT, 'X',curses.color_pair(2))
 
             stdscr.refresh()
 
